66c6bd629ac8c70ebf6a2464bef106a0cf861470
Teddy Byron <ted@tedbyron.com>
2024-02-09 22:27:11 -0500

    Add credo dep

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Phooey.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
